b5645f5fef254eed17c10b51c9b6f6465333f6e6
Chad Danford <chad.danford@calumetphoto.de>
2024-11-11 13:30:04 +0100

    nice nice nice

--- about.markdown
+++ about.markdown
@@ -4,7 +4,16 @@ title: About
 permalink: /about/
 ---
 
-This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at [jekyllrb.com](https://jekyllrb.com/)
+This is the base Jekyll theme.
+
+I'm baby fam post-ironic man braid intelligentsia snackwave, gorpcore Brooklyn fit la croix hoodie vape. Tacos irony lyft, marxism intelligentsia same sustainable twee hell of kombucha sartorial banjo listicle. JOMO green juice praxis iceland tumblr leggings cray migas gluten-free fingerstache food truck hella tbh pour-over. Migas mumblecore bicycle rights, microdosing umami butcher intelligentsia unicorn neutra post-ironic keffiyeh. Artisan JOMO blackbird spyplane you probably haven't heard of them hexagon, small batch seitan banh mi thundercats pickled.
+
+Schlitz brunch big mood offal typewriter. Bicycle rights shabby chic edison bulb succulents godard, typewriter vinyl art party. Cold-pressed air plant la croix semiotics tbh. Butcher mixtape activated charcoal chia post-ironic put a bird on it hashtag migas whatever thundercats.
+
+Raclette tumblr flexitarian sustainable. Chicharrones thundercats unicorn chartreuse pork belly tumeric ascot small batch prism try-hard YOLO fam heirloom. Iceland tonx echo park viral letterpress biodiesel YOLO celiac keytar banh mi shoreditch vibecession raw denim ethical jawn. Williamsburg letterpress yr affogato, hexagon cloud bread knausgaard chambray taxidermy. Raw denim pickled knausgaard vice, sartorial gastropub affogato vegan succulents wayfarers unicorn leggings neutra waistcoat fit. Distillery VHS tofu flannel keytar pop-up stumptown tattooed echo park offal art party mukbang tumblr plaid. Fanny pack portland readymade organic quinoa raw denim bespoke health goth s
+
+
+You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at [jekyllrb.com](https://jekyllrb.com/)
 
 You can find the source code for Minima at GitHub:
 [jekyll][jekyll-organization] /
--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,86 +1,40 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum;
-    var fullTxt = this.toRotate[i];
-
-    // Only typing, no deleting
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-    var that = this;
-    var delta = 100 - Math.random() * 50; // Speed up typing
-
-    // If full text is reached, move to the next one after a pause
-    if (this.txt === fullTxt) {
-        delta = this.period;
-        this.loopNum++;
-        
-        // Stop if we’ve reached the end of all texts
-        if (this.loopNum >= this.toRotate.length) {
-            return;
-        }
-        
-        // Reset txt for the next text in array
-        this.txt = '';
-    }
-
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
-
-document.addEventListener('click', function(event) {
-    if (event.target.closest('.burger') || event.target.closest('.menu-item')) {
-        document.querySelector('.burger').classList.toggle('open');
-        
-        const overlay = document.querySelector('.overlay');
-        if (overlay.style.display === 'block') {
-            overlay.style.display = 'none';
-        } else {
-            overlay.style.display = 'block';
-        }
-
-        const nav = document.querySelector('.nav');
-        nav.classList.toggle('navactive');
-        nav.classList.toggle('navhidden');
-    }
-
-    if (event.target.closest('.back')) {
-        window.location.href = 'https://www.chadmdanford.com';
-    }
-});
-
-
 document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const overlay = document.querySelector(".overlay");
+    const nav = document.querySelector(".nav");
+    const burgerNav = document.getElementById("mobile-nav-header");
     let lastScrollTop = 0;
-    const burgerNav = document.getElementById("nav-icon");
   
+    document.addEventListener("click", function (event) {
+      if (event.target.closest(".burger") || event.target.closest(".menu-item")) {
+        // Toggle burger open state
+        burger.classList.toggle("open");
+  
+        // Toggle overlay visibility
+        if (overlay.style.display === "block") {
+          overlay.style.display = "none";
+          document.body.style.overflow = "auto"; // Re-enable scrolling
+        } else {
+          overlay.style.display = "block";
+          document.body.style.overflow = "hidden"; // Disable scrolling
+        }
+  
+        // Toggle navigation classes
+        nav.classList.toggle("navactive");
+        nav.classList.toggle("navhidden");
+      }
+  
+      // If clicking on the back button
+      if (event.target.closest(".back")) {
+        window.location.href = "https://www.chadmdanford.com";
+      }
+    });
+  
+    // Scroll function, runs only if overlay is not active
     window.addEventListener("scroll", () => {
-      const scrollTop = window.screenY || document.documentElement.scrollTop;
+      if (overlay.style.display === "block") return; // Skip scroll handling if overlay is active
+  
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
   
       if (scrollTop > lastScrollTop) {
         // User scrolled down - hide navigation
